73273c324ba27eb07af62afc8a482f5c972077ec
Oliver <o.copleston@gmail.com>
2017-03-28 10:51:35 +0100

    finished task 2

--- cfg.py
+++ cfg.py
@@ -1,7 +1,8 @@
 import cfg_reader
-import pprint as pprint
+from pprint import pprint
 from collections import OrderedDict
 import string
+import itertools
 
 class CFG():
 	def __init__(self, v_in, t_in, r_in, s_in):
@@ -10,6 +11,11 @@ class CFG():
 		self.rules_0 = r_in
 		self.start_0 = s_in
 		self.rules = OrderedDict([("S0", "")])
+
+		self.A_ref = {}
+		self.A_dict = OrderedDict()
+		self.U_ref = {}
+		self.U_dict = OrderedDict()
 		# Set up a dictionary of rules, which combines the productions for rules with the same variable
 		self.in_dict = OrderedDict([(k,[]) for k in self.variables])
 		self.rules.update(self.in_dict)
@@ -26,13 +32,20 @@ class CFG():
 		self.unit_rules()
 		self.print_rules()
 
-		# ** CONVERT REMAINING RULES **
-		print(20*'-', "CONVERT REMAINING RULES", 20*'-')
-		print(20*'-', "VARIABLES", 20*'-')
-		self.replace_variables()
+		print(20*'-', "REMOVE EMPTY SET RULES", 20*'-')
+		self.remove_e_rules()
 		self.print_rules()
-		print(20*'-', "TERMINALS", 20*'-')
-		self.replace_terminals()
+		self.unit_rules()
+
+		#** CONVERT REMAINING RULES **
+		print(20*'-', "CONVERT REMAINING RULES", 20*'-')
+		while self.valid() is False:
+			self.remove_e_rules()
+			self.replace_variables()
+			self.replace_terminals()
+			self.print_rules()
+
+		print(20*'-', "CFG IN CHOMSKY NORMAL FORM", 20*'-')
 		self.print_rules()
 
 
@@ -50,6 +63,38 @@ class CFG():
 			else:
 				print(k, "->", v)
 
+	def valid_rule_1(self, v):
+		if len(v) == 2 and (set(v).issubset(self.variables)):
+			# print("VALID 1")
+			return True
+
+	def valid_rule_2(self, v):
+		if len(v) == 1 and v in self.terminals:
+			# print("VALID 2")
+			return True
+
+	def valid_rule_3(self, k, v):
+		if k == self.start_0 and v == "e":
+			# print("VALID 3")
+			return True
+
+	def valid(self):
+		"""Perform validation checks on each of the rules
+		valid_rule_1 = A -> BC
+		valid_rule_2 = A -> x
+		valid_rule_3 = S -> e
+		"""
+		for k, v in self.rules.items():
+			for i in v:
+				if any([self.valid_rule_1(i), self.valid_rule_2(i), self.valid_rule_3(k, i)]):
+					# print("Got a pass")
+					pass
+				else:
+					# print("Got a fail")
+					return False
+		# print("CORRECT CFG")
+		return True
+
 	def new_start_var(self):
 		self.rules["S0"] = self.rules[self.start_0]
 
@@ -60,96 +105,79 @@ class CFG():
 					self.rules[k].remove(i)
 					self.rules[k].extend(self.rules[i])
 
-	def replace_variables(self):
-		# A_count = 0
-		A_ref = {}
-		A_dict = OrderedDict()
+	def remove_e_rules(self):
+		e_vars = []
+		e_dict = OrderedDict()
+		for k, v in self.rules.copy().items():
+			for x in v:
+				if x == "e":
+					e_vars.append(k)
+		for k, v in self.rules.copy().items():
+			e_set = []
+			for i, x in enumerate(v):
+				if len(x) != 1:
+					if any(j in list(x) for j in e_vars):
+						y = list(x)
+						for l in range(1, len(x)):
+							for subset in itertools.combinations(y, l):
+								if (any(subset) not in e_vars):
+									if not set(subset).issubset(e_vars):
+										e_set.append("".join(list(subset)))
 
+			if len(e_set) > 0:
+				e_dict[k] = list(set(e_set) - set(self.variables))
+			# print(e_set)
+
+		for i in e_dict.keys():
+			self.rules[i].extend(e_dict[i])
+			self.rules[i] = list(set(self.rules[i]))
+
+		# Remove all "e"s from the CFG
+		for k, v in self.rules.items():
+			for i in v:
+				if i == "e" and k != self.start_0:
+					self.rules[k].remove(i)
+
+	def replace_variables(self):
 		for k, v in self.rules.copy().items():
 			for i, x in enumerate(v):
-				if len(x) > 1:
-					if x[:-1] in A_ref.keys():
-						self.rules[k][i] = A_ref[x[:-1]] + x[-1]
-					else:
-						val = self.alpha.pop(0)
-						A_ref[x[:-1]] = val
-						A_dict[val] = []
-						A_dict[val].append(x[:-1])
-						self.variables.append(val)
+				if len(x) > 1 and (len(x) != 2 and all(list(x)) not in self.variables):
+						if x[:-1] in self.A_ref.keys():
+							self.rules[k][i] = self.A_ref[x[:-1]] + x[-1]
+						else:
+							val = self.alpha.pop(0)
+							self.A_ref[x[:-1]] = val
+							if any(list(x[:-1])) not in self.variables:
+								self.rules[k][i] = val + x[-1]
+							self.A_dict[val] = []
+							self.A_dict[val].append(x[:-1])
+							self.variables.append(val)
 
-		self.rules.update(A_dict)
+		self.rules.update(self.A_dict)
 
 	def replace_terminals(self):
-		U_ref = {}
-		U_dict = OrderedDict()
-
 		for k, v in self.rules.copy().items():
 			for i, x in enumerate(v):
 				if len(x) != 1:
 					for j in x:
 						if j in self.terminals:
-							if j in U_ref.keys():
-								self.rules[k][i] = self.rules[k][i].replace(j, U_ref[j])
+							if j in self.U_ref.keys():
+								self.rules[k][i] = self.rules[k][i].replace(j, self.U_ref[j])
 							else:
 								val = self.alpha.pop(0)
-								U_ref[j] = val
-								U_dict[val] = []
-								U_dict[val].append(j)
+								self.U_ref[j] = val
+								self.U_dict[val] = []
+								self.U_dict[val].append(j)
 								self.variables.append(val)
-								self.rules[k][i] = self.rules[k][i].replace(j, U_ref[j])
-		self.rules.update(U_dict)
-
-
-class Var():
-	def __init__(self):
-		pass
-
-def valid(x):
-	if len(x) == 2:
-		if (x[0] in self.variables) and (x[1] in self.variables):
-			return True
-	elif len(x) == 1:
-		if (x in self.terminals) or (x == "e"):
-			return True
-	else:
-		return False
-
-class Rule():
-	def __init__(self):
-		pass
-
-
-	var = ""
-	production = []
-
-class Production():
-	def __init__(self, v_in):
-		self.var = v_in
-
-	def check_in_cnf(self):
-		if len(x) == 2:
-			if (x[0] in self.variables) and (x[1] in self.variables):
-				return True
-		elif len(x) == 1 and x in self.terminals:
-			return True
-		elif
-
-for i in productions:
-	if rule.var == start_variable and production == e:
-		return True
-	elif all(list(i) in variables):
+								self.rules[k][i] = self.rules[k][i].replace(j, self.U_ref[j])
+		self.rules.update(self.U_dict)
 
 
 
-	def refactor(self):
-		pass
-
-	def var_replace(self, this, that):
-		pass
-
-
-
-cfg = cfg_reader.File_reader("G0.txt")
+cfg = cfg_reader.File_reader("G1.txt")
 cfg.print_cfg()
 G0_in = cfg.get_cfg()
 G0 = CFG(G0_in["variables"], G0_in["terminals"], G0_in["rules"], G0_in["start"])
+# w = input("Enter a string: ")
+# w_len = len(w)
+# print(w_len)
